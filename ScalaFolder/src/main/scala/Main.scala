3956f544bc10a612f63924994fa6f57f715ba00f
jerry <jerry.sun@totalimagine.com>
2021-03-12 14:12:56 -0500

    create offical project code

---
+++ ScalaFolder/src/main/scala/Main.scala
@@ -0,0 +1,18 @@
+import MongoDB.MongoDBDriver.connectSentiment140
+
+object Main {
+  def main(args: Array[String]): Unit = {
+    println("Apache Spark Application Started ...")
+
+    // get the uri from MongoDB Compass
+    val uri: String = "mongodb://admin:Z7aDPBAx9GjWJw@cluster0-shard-00-00.k9dyo.mongodb.net:27017," +
+      "cluster0-shard-00-01.k9dyo.mongodb.net:27017,cluster0-shard-00-02.k9dyo.mongodb.net:27017/LearnMongoDB?ssl=" +
+      "true&replicaSet=atlas-1ek15t-shard-0&authSource=admin&retryWrites=true&w=majority"
+
+    // get the sentiment140 DataFrame
+    val sentiment140_DF = connectSentiment140(uri)
+
+
+    println("Apache Spark Application Completed.")
+  }
+}
---
+++ ScalaFolder/src/main/scala/MongoDB/MongoDBDriver.scala
@@ -0,0 +1,28 @@
+package MongoDB
+
+import org.apache.spark.SparkContext
+import org.apache.spark.sql.{DataFrame, SparkSession}
+import org.mongodb.scala.{MongoClient, MongoCollection, MongoDatabase}
+import com.mongodb.spark.config._
+import com.mongodb.spark._
+import org.apache.spark.sql.functions.{col, udf}
+import org.apache.spark.sql.types.{StructType, StructField}
+
+object MongoDBDriver {
+  def connectSentiment140(uri: String): DataFrame = {
+    /* Create the SparkSession.
+    */
+    val spark = SparkSession.builder()
+      .master("local")
+      .appName("MongoSparkConnectorIntro")
+      .config("spark.mongodb.input.uri", uri)
+      .config("spark.mongodb.output.uri", uri)
+      .getOrCreate()
+
+    val sc = spark.sparkContext
+    val loadConfig = ReadConfig(Map("uri"->uri, "collection" -> "TwitterSentimentProject", "database" -> "LearnMongoDB"))
+    import spark.implicits._
+    val rdd = MongoSpark.load(sc, loadConfig).toDF()
+    rdd
+  }
+}
