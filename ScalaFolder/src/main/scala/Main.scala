8974922daad3f344745e0ae3979c852cdabbfbe3
jerry <jerry.sun@totalimagine.com>
2021-03-15 17:26:06 -0400

    work on spark streaming

--- ScalaFolder/src/main/scala/Main.scala
+++ ScalaFolder/src/main/scala/Main.scala
@@ -1,29 +1,33 @@
 import MongoDB.MongoDBDriver.{connectCollection}
 import SparkNLP.SparkNLPDriver._
+import SparkStreaming.SparkStreamingDriver._
 
 object Main {
   def main(args: Array[String]): Unit = {
     println("Apache Spark Application Started ...")
 
     // get the uri from MongoDB Compass
-    val uri: String = "mongodb://admin:Z7aDPBAx9GjWJw@cluster0-shard-00-00.k9dyo.mongodb.net:27017," +
-      "cluster0-shard-00-01.k9dyo.mongodb.net:27017,cluster0-shard-00-02.k9dyo.mongodb.net:27017/LearnMongoDB?ssl=" +
-      "true&replicaSet=atlas-1ek15t-shard-0&authSource=admin&retryWrites=true&w=majority"
+//    val uri: String = "mongodb://admin:Z7aDPBAx9GjWJw@cluster0-shard-00-00.k9dyo.mongodb.net:27017," +
+//      "cluster0-shard-00-01.k9dyo.mongodb.net:27017,cluster0-shard-00-02.k9dyo.mongodb.net:27017/LearnMongoDB?ssl=" +
+//      "true&replicaSet=atlas-1ek15t-shard-0&authSource=admin&retryWrites=true&w=majority"
 
     // collection name
-    val colletionName = "Sentiment140"
+//    val colletionName = "Sentiment140"
 //    val colletionName = "TestCollection"
 
     // get the DataFrame from MongoDB
-    val sentiment_DF = connectCollection(uri,colletionName)
+//    val sentiment_DF = connectCollection(uri,colletionName)
 
     // get the sentiment summary for sentiment140 dataset
 //    val sentiment_summary = getSentimentSummary(sentiment_DF)
 //    sentiment_summary.show()
 
     // compute the accuracy of spark nlp sentiment prediction
-    val accuracy = computeSparkNLPAccuracy(sentiment_DF)
-    println(s"Spark-nlp Accuracy: $accuracy")
+//    val accuracy = computeSparkNLPAccuracy(sentiment_DF)
+//    println(s"Spark-nlp Accuracy: $accuracy")
+
+    //Spark Streaming
+    runSparkStreaming()
 
     println("Apache Spark Application Completed.")
   }
