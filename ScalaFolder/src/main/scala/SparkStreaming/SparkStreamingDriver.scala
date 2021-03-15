dc215b468a07b4ec95f7800fcd455e0337ccb089
jerry <jerry.sun@totalimagine.com>
2021-03-15 14:34:34 -0400

    push before today job

--- ScalaFolder/src/main/scala/SparkStreaming/SparkStreamingDriver.scala
+++ ScalaFolder/src/main/scala/SparkStreaming/SparkStreamingDriver.scala
@@ -1,5 +1,9 @@
 package SparkStreaming
 
 object SparkStreamingDriver {
+  // Access token: 1368376671733178372-inXzPbhkwXNnS56wx5NihMDFc7FM5D
+  // Access token secret: 1wWxyYyASGwSmrzFr3BRxI4QiVKJ4SAttnTiNIE2391OW
 
+  // API key: ENGYp3Uh5L9xxDuE8TjIl1hzZ
+  // API key secret: WSLLdjomzKnLOlCszmI4pTY6noC0630BlM2l1rhuU1fJoULPbo
 }
